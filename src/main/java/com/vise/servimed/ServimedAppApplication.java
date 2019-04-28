f1ceb3dcb8fc10afb2640aef4b2118bf1743eb99
elias.rosa <elias.rosa@grupovasport.net>
2019-04-28 09:10:29 -0600

    first commit

---
+++ src/main/java/com/vise/servimed/ServimedAppApplication.java
@@ -0,0 +1,13 @@
+package com.vise.servimed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServimedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServimedAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vise/servimed/ServimedAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vise.servimed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServimedAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
